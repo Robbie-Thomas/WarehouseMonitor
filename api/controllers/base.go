5eec97fd8801bd116ae2b18276a10f52c7986bd6
robbie <robbiejt55@gmail.com>
2020-01-17 17:15:22 +0000

    Created DB seeder and server.go, main.go to run the api
    Signed-off-by: robbie <robbiejt55@gmail.com>

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -17,7 +17,7 @@ type Server struct {
 }
 
 func (server *Server) Initialiser(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
-	err := error()
+	var err error
 	if Dbdriver == "mysql" {
 		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
 		server.DB, err = gorm.Open(Dbdriver, DBURL)
