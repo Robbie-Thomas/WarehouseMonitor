cc65f13de8a61dafafce24958919799155e2ed8e
robbie <robbiejt55@gmail.com>
2020-01-17 16:39:58 +0000

    Created login and post controller
    Signed-off-by: robbie <robbiejt55@gmail.com>

--- api/models/User.go
+++ api/models/User.go
@@ -24,6 +24,10 @@ func Hash(password string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 }
 
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
 func (u *User) BeforeSave() error {
 	hashedPassword, err := Hash(u.Password)
 	if err != nil {
